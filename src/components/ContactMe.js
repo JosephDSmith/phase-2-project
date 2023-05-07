064baa7e40c608cd2600fcc2dcdb64db1cc30951
Joseph Smith <joesmith40@gmail.com>
2023-05-07 13:18:39 -0500

    added css

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,17 +1,26 @@
-
-
 function ContactMe() {
-
-    return(
-        <div>
-            <h3>You can find me on</h3>
-            <a href="https://github.com/JosephDSmith" target="_blank">Github</a>
-            <a href="https://www.linkedin.com/in/joseph-d-smith-692522264/" target="_blank">LinkedIn</a>
-            <a href="https://medium.com/@joesmith40" target="_blank">Medium</a>
-            <a href="mailto:joesmith40@gmail.com">Click HERE to email me!</a>
-        </div>
-    )
-
+  return (
+    <div className="contact-me">
+      <h3>You can find me on</h3>
+      <div className="contact-me-links">
+        <a href="https://github.com/JosephDSmith" target="_blank">
+          <i className="fa-brands fa-github"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/joseph-d-smith-692522264/"
+          target="_blank"
+        >
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="https://medium.com/@joesmith40" target="_blank">
+          <i className="fa-brands fa-medium"></i>
+        </a>
+        <a href="mailto:joesmith40@gmail.com">
+          <i className="fa-solid fa-envelope"></i>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default ContactMe
+export default ContactMe;
