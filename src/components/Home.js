3b37add3bc1dddb4793bcc3adfbd8782c3356f01
Joseph Smith <joesmith40@gmail.com>
2023-05-08 22:40:51 -0500

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,151 @@
 function Home() {
   return (
     <div className="home-container">
-      <img src="https://cdn.create.vista.com/api/media/small/568394364/stock-photo-illustration-big-bonsai-tree-isolated"/>
-      <p>Welcome to my Bonsai Collection!
-      Please have a look around my app - enjoy!
+      <div className="hero-section">
+        <div className="hero-details">
+          <h1>
+            Always looks for the smallest
+            <br />
+            tree within the tree
+          </h1>
+          <p>
+            A bonsai tree is a living tree or shrub which has been grown in a
+            way which gives the impression of being a full-sized, mature tree.
+            An artistic representation of a full-sized tree in nature. Bonsai
+            trees are generally grown in shallow ceramic pots.
+          </p>
+        </div>
+        <div className="hero-img">
+          <img src="https://img.freepik.com/premium-photo/five-needle-pine-bonsai-with-white-background_631068-1305.jpg" />
+        </div>
+      </div>
+      <h2>Bonsai Tree Care for Beginners</h2>
+      <h3>How to Position Your Bonsai Tree</h3>
+      <img
+        src="https://www.ftd.com/blog/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fi3tkg7dt3kro%2F3fWdymwixFtNQ2hMPRGnWm%2Ffd44d609d893368b6af045bbe8eb1fd6%2Fpositioning-bonsai.jpg&w=1920&q=75"
+        alt="position"
+      />
+      <p>
+        To find the perfect location for your bonsai, it's crucial to identify
+        the type of tree you have and its ideal environment. For outdoor bonsai,
+        like juniper and pine, exposure to the seasons is essential, while
+        deciduous trees like elms and maples also thrive outdoors. Indoor
+        bonsai, such as jade and ficus trees, require stable temperatures
+        throughout the year to thrive.
       </p>
-      <p></p>
+      <p>
+        Once you have identified the type of bonsai you have, you can select the
+        best location for it. Outdoor bonsai will need to be placed in areas
+        with appropriate sun exposure and airflow, while indoor bonsai will
+        require a stable environment with indirect sunlight. Proper placement
+        will ensure that your bonsai thrives and brings beauty to your space.
+      </p>
+      <img
+        src="https://www.ftd.com/blog/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fi3tkg7dt3kro%2F41RHKuTKyAjNhJ1nqVps3O%2F62564a858c6a0456d2834bdae41e63cf%2Fbonsai-types.jpg&w=1920&q=75"
+        alt="type"
+      />
+      <p>
+        Once you’ve figured out what type of bonsai tree you have, the rest is
+        fairly simple. Here are some general tips on bonsai tree positioning
+        that typically apply to all types of bonsai trees.
+      </p>
+      <div>
+        <p>
+          Positioning: Your bonsai should be kept away from direct heat or
+          draft.
+        </p>
+        <p>Lighting: Keep your bonsai in area with plenty of sunlight.</p>
+        <p>
+          Humidity: Bonsais need humidity in order to keep their soil moist.
+        </p>
+      </div>
+      <h3>Watering Your Bonsai Tree</h3>
+      <img
+        src="https://www.ftd.com/blog/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fi3tkg7dt3kro%2F4uzjMSjmRYe90n7yjJDDWU%2Fb9da189d881d3c178c488e202a6220bc%2Fwatering-bonsai.jpg&w=1920&q=75"
+        alt="watering"
+      />
+      <p>
+        The primary cause of bonsai tree death is often due to under-watering.
+        Because of the shallow soil layer, it can dry out quickly. To prevent
+        this, it's important to water your bonsai tree as soon as the top layer
+        of soil appears dry. Depending on the size and type of your bonsai tree,
+        watering frequency can vary and may need to be done daily. Therefore,
+        it's essential to water each bonsai plant individually, rather than
+        sticking to a set routine.
+      </p>
+      <p>
+        When watering your bonsai tree, the goal is to saturate the root system
+        fully. You'll know you've done this when water escapes through the
+        draining holes. Many bonsai trees come with a tray to collect excess
+        water to allow for proper draining. Overwatering can be just as harmful
+        to your bonsai tree as under-watering, resulting in yellowing leaves and
+        shriveling branches. To ensure proper watering, assess your bonsai tree
+        daily and water it as soon as the soil appears dry.
+      </p>
+
+      <h3>Pruning and Shaping Your Bonsai Tree</h3>
+      <img
+        src="https://www.ftd.com/blog/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fi3tkg7dt3kro%2F5wr5JGiNziIHHtzU6G5cBw%2F6162627dab521505430091d26b1f448f%2Fpruning-bonsai.jpg&w=1920&q=75"
+        alt="pruning"
+      />
+      <p>
+        Pruning is a crucial aspect of bonsai tree maintenance, necessary to
+        keep the tree small and maintain its compact shape. There are two types
+        of pruning: maintenance pruning and structural pruning.
+      </p>
+      <p>
+        Maintenance pruning stimulates new growth and strengthens the tree by
+        cutting away young shoots and leaves to expose the leaves underneath to
+        air and sunlight. Pruning branches encourages the growth of smaller
+        branches and allows for greater control of the tree's shape. Cutting
+        away buds from branches produces a more compact leaf growth that
+        encourages smaller leaves.
+      </p>
+      <p>
+        Pruning should be done when you notice new growth that is starting to
+        morph the tree's shape in an undesirable way. For flowering bonsais,
+        prune during spring to encourage more flowers the following year.
+        Structural pruning is an advanced technique best done during the dormant
+        season and requires the skills of a professional to ensure the tree can
+        recover properly. Another method of shaping the bonsai tree is wiring
+        its branches during winter when leaves have fallen off. Keep an eye on
+        the branch's growth and remove the wire when necessary to avoid
+        scarring.
+      </p>
+      <h3>Choosing the Right Soil For Your Bonsai</h3>
+      <img
+        src="https://www.ftd.com/blog/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fi3tkg7dt3kro%2F4o071gl51U7Wss9L96Kl6n%2F6f93eb12115b1817d5cf2d3fbdf02fff%2Fsoil-for-bonsai.jpg&w=1920&q=75"
+        alt="soil"
+      />
+      <p>
+        The key to choosing the right soil for your bonsai is to choose one that
+        offers proper drainage. Add large particles to your soil mixture, such
+        as volcanic rock or stones, to improve drainage and to introduce air
+        into the soil. The ideal soil mixture should also be able to hold water
+        which can be improved by adding clay.
+      </p>
+      <p>
+        Fertilizing your bonsai ensures that it receives the proper amount of
+        nutrients it needs to stay healthy. A balanced bonsai fertilizer
+        contains equal amounts of nitrogen, phosphorus and potassium. Before you
+        fertilize, make sure that you’ve watered your tree thoroughly as it is
+        harmful to fertilize the plant while the soil is dry. Be sure to read
+        the instructions on the fertilizer to avoid overfertilizing.
+      </p>
+      <h3>Repotting Your Bonsai</h3>
+      <img
+        src="https://www.ftd.com/blog/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fi3tkg7dt3kro%2F2XTaAvIdcGJIpGwP1wiG9y%2F7d4fea32ce8d2a5b7514f43cf4989e91%2Frepotting-bonsai.jpg&w=1920&q=75"
+        alt="repotting"
+      />
+      <p>
+        Repotting is a key factor in maintaining the health of your bonsai tree.
+        The purpose of repotting is to remove excess roots which can cause the
+        tree to starve, or not receive enough nutrients for its mass. Repotting
+        also ensures that your tree can continue to thrive within a small pot.
+        Bonsai trees should be repotted once every two to five years, depending
+        on how quickly your bonsai tree grows.
+      </p>
+
     </div>
   );
 }
